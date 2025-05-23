6870abd3f35d5113af2968d8b900f350b833aaf5
adityadorge <adityadorge07@gmail.com>
2025-05-23 15:42:25 +0530

    first commit

---
+++ create_tables.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IS NOT EXISTS demo;
+USE demo;
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100),
+    marks INT
+);
+
+INSERT INTO students (user_name,marks) VALUES
+('omkar',85),
+('pratham',90),
+('mayur',71),
+('pranav',0),
+('aditya',100)
